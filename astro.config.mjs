d5bbd5e0117f87536d5e0a85894e3b6e8c10c6cd
Ignacio Yuste Lopez <iyustlop@gmail.com>
2024-10-01 23:05:40 +0200

    add repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://iyustlop.github.io',
+  base: 'iyustlop.github.io',
   integrations: [tailwind()]
 });
