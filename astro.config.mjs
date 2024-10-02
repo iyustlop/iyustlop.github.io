1491c41b5faefc96ac9aa5cd5c112ea8f45ed2e0
iyustlop <Log1t3ch>
2024-10-02 11:59:40 +0200

    creation of owner info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
   integrations: [tailwind()]
 });
